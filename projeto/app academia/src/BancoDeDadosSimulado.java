47fd0c6bb58094df7a1202a593f8d686bfff7cc0
nalvesleonardo <leonardoalves6558@gmail.com>
2024-07-15 18:49:02 -0300

    final

--- projeto/app academia/src/BancoDeDadosSimulado.java
+++ projeto/app academia/src/BancoDeDadosSimulado.java
@@ -3,13 +3,11 @@ import java.util.Map;
 
 public class BancoDeDadosSimulado {
     private Map<String, Usuario> usuarios;
-    private Professor professor;
 
     public BancoDeDadosSimulado() { //usuarios pre prontos
         usuarios = new HashMap<>();
         usuarios.put("madu", new Aluno("Maria Eduarda", "1234", "mariaeduarda2024@gmail.com", "51998540392", true));
         usuarios.put("carlos", new Aluno("Carlos Arenques", "1234", "carlosarenques@gmail.com", "51997548327", false));
-
     }
 
     public Usuario getUsuario(String nome) {
@@ -21,9 +19,5 @@ public class BancoDeDadosSimulado {
             throw new IllegalArgumentException("Usuário já existe.");
         }
         usuarios.put(usuario.getNome(), usuario);
-    }
-
-    public Professor getProfessor() {
-        return professor;
     }
 }
